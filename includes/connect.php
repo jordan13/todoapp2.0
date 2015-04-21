2c8fc855c6bbac96ca43169aef3a0f9ca0654512
Jordan Abujaber <jajaber13@gmail.com>
2015-04-21 08:47:19 -0700

    Video 5.1 done

--- includes/connect.php
+++ includes/connect.php
@@ -1,8 +1,8 @@
 <?php
 // why is this important ? why does localhost have to be first ? 
-$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
 // if $mysqli its a connect errno ($mysqli->connect_error) then we want it to DIE and have this message
-if (mysqli->connect_error) {
+if ($mysqli->connect_error) {
 	die('Connect Error (' . $mysqli->connect_errno . ')'
 		. $mysqli->connect_error);
 }
